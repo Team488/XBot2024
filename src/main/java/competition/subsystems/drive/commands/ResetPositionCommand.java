a5225bf177b0ceb9e329b4a8e68ace4fd5776f2b
anthonytrenh <trenhanthony@gmail.com>
2024-01-18 19:49:39 -0800

    Set min and max limits to positional and heading pid
    Also created a command class to reset position and a command class to translate forward

---
+++ src/main/java/competition/subsystems/drive/commands/ResetPositionCommand.java
@@ -0,0 +1,23 @@
+package competition.subsystems.drive.commands;
+
+import xbot.common.command.BaseCommand;
+
+import javax.inject.Inject;
+
+public class ResetPositionCommand extends BaseCommand {
+
+    @Inject
+    public ResetPositionCommand() {
+
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ src/main/java/competition/subsystems/drive/commands/TranslateForwardCommand.java
@@ -0,0 +1,22 @@
+package competition.subsystems.drive.commands;
+
+import xbot.common.command.BaseCommand;
+
+import javax.inject.Inject;
+
+public class TranslateForwardCommand extends BaseCommand {
+    @Inject
+    public TranslateForwardCommand() {
+
+    }
+    
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
