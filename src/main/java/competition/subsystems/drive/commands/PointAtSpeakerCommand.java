5e80fff769f32cf5951bd7dd9ab3f0f079f8cf2a
junyu101 <junyuzhu2007@gmail.com>
2024-01-20 12:07:18 -0800

    Update PointAtSpeakerCommand.java

--- src/main/java/competition/subsystems/drive/commands/PointAtSpeakerCommand.java
+++ src/main/java/competition/subsystems/drive/commands/PointAtSpeakerCommand.java
@@ -3,6 +3,7 @@ package competition.subsystems.drive.commands;
 import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.pose.PoseSubsystem;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import xbot.common.command.BaseCommand;
 import xbot.common.math.XYPair;
 
@@ -18,25 +19,28 @@ public class PointAtSpeakerCommand extends BaseCommand {
 
     Pose2d currentPosition;
     double angle;
-    HeadingModule headingModule;
+    XYPair currentPositionCord;
 
-    public PointAtSpeakerCommand(DriveSubsystem drive, PoseSubsystem pose, HeadingModule.HeadingModuleFactory headingModuleFactory){
+    public PointAtSpeakerCommand(DriveSubsystem drive, PoseSubsystem pose){
         this.drive = drive;
         this.pose = pose;
 
     }
 
 
+
     @Override
     public void initialize() {
     currentPosition = pose.getCurrentPose2d();
+    currentPositionCord = new XYPair(currentPosition.getX(),currentPosition.getY());
     }
 
     @Override
     public void execute() {
         currentPosition = pose.getCurrentPose2d();
-
+        currentPositionCord.x = currentPosition.getX();
+        currentPositionCord.y = currentPosition.getY();
         angle = (90 + (180 - Math.tan((currentPosition.getX() - speakerPosition.x) / (currentPosition.getY() - speakerPosition.y))));
-        drive.fieldOrientedDrive();
+        drive.fieldOrientedDrive(currentPositionCord,angle,pose.getCurrentHeading().getDegrees(),false);
     }
 }
