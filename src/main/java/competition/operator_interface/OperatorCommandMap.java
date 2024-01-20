83b14053bc6bf71a2a486d26fa24a582ca124865
junyu101 <junyuzhu2007@gmail.com>
2024-01-20 11:34:16 -0800

    Commits before changges

--- src/main/java/competition/operator_interface/OperatorCommandMap.java
+++ src/main/java/competition/operator_interface/OperatorCommandMap.java
@@ -73,7 +73,7 @@ public class OperatorCommandMap {
         speakerPoints.add(XbotSwervePoint.createXbotSwervePoint(
                 new Translation2d(5.7,6.3), Rotation2d.fromDegrees(190), 10));
         speakerPoints.add(XbotSwervePoint.createXbotSwervePoint(
-                new Translation2d(3.7,2.429256), Rotation2d.fromDegrees(90 + ), 10));
+                new Translation2d(3.7,2.429256), Rotation2d.fromDegrees(132), 10));
         pointAtSpeaker.logic.setKeyPoints(speakerPoints);
         pointAtSpeaker.logic.setEnableConstantVelocity(true);
         pointAtSpeaker.logic.setConstantVelocity(2);
--- src/main/java/competition/subsystems/drive/commands/PointAtSpeakerCommand.java
+++ src/main/java/competition/subsystems/drive/commands/PointAtSpeakerCommand.java
@@ -2,22 +2,33 @@ package competition.subsystems.drive.commands;
 
 import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.pose.PoseSubsystem;
+import edu.wpi.first.math.geometry.Pose2d;
 import xbot.common.command.BaseCommand;
 import xbot.common.math.XYPair;
+import xbot.common.subsystems.drive.control_logic.HeadingModule;
+import java.lang.Math;
 
 public class PointAtSpeakerCommand extends BaseCommand {
 
     PoseSubsystem pose;
     DriveSubsystem drive;
     XYPair speakerPosition = new XYPair(-0.0381,5.547868);
+    Pose2d currentPosition;
+    HeadingModule headingModule;
+
+    public PointAtSpeakerCommand(){
+        
+    }
+
     double angle;
     @Override
     public void initialize() {
-
+    currentPosition = pose.getCurrentPose2d();
     }
 
     @Override
     public void execute() {
-        angle = pose.
+        currentPosition = pose.getCurrentPose2d();
+        angle = (90 + (180 - Math.tan((currentPosition.getX() - speakerPosition.x) / (currentPosition.getY() - speakerPosition.y))));
     }
 }
