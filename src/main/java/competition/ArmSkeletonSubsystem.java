114efa7ecbfabb3083098b2845a605a0bf2f7801
SHORT-LIFE-CHOSE-ME <118636986+SHORT-LIFE-CHOSE-ME@users.noreply.github.com>
2024-01-20 08:39:42 -0800

    Update ArmSkeletonSubsystem.java

--- src/main/java/competition/ArmSkeletonSubsystem.java
+++ src/main/java/competition/ArmSkeletonSubsystem.java
@@ -1,54 +1,59 @@
-package competition.subsystems.drive.commands;
+package competition;
 
+import xbot.common.controls.actuators.XCANSparkMax;
 import edu.wpi.first.wpilibj.XboxController;
-public class RobotArm2024 {
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
+public class ArmSkeletonSubsystem {
 
-    motor motor1;
-    motor motor2;
-    private XboxController controller;
-    public double armEncoder;
+    public XCANSparkMax motor1;
+    public XCANSparkMax motor2;
+    public XboxController controller;
 
+    // Arm movements
+    double ARM_UP_SPEED = 0.5;
+    double ARM_DOWN_SPEED = -0.5;
+    double DESIRED_ANGLE = 45.0; // don't forget to change this when you want w
 
-    //        Arm movements
-    private static final double ARM_UP_SPEED = 0.5;
-    private static final double ARM_DOWN_SPEED = -0.5;
-    private static final double DESIRED_ANGLE = 45.0; // don't forget to change this when you want w
+    public class ManualArmControlCommand extends CommandBase {
+        @Override
+        public void execute() {
+            double joystickY = -controller.getLeftY();
 
+            motor1.set(joystickY);
+            motor2.set(joystickY);
 
+            // Move the arm to a wanted angle
+            double currentAngle = armEncoder();
 
-    public void initialize() {
-
-        motor1 = new motor();
-        motor2 = new motor();
-        controller = new XboxController(0);
-
-        double joystickY = -controller.getLeftY();
-
-
-        motor1.set(joystickY);
-        motor2.set(joystickY);
-        // Move the arm to a wanted angle
-        double currentAngle;
-        currentAngle = armEncoder();
-        if (currentAngle < DESIRED_ANGLE) {
-            // Move arm up
-            motor1.set(ARM_UP_SPEED);
-            motor2.set(ARM_UP_SPEED);
-        } else if (currentAngle > DESIRED_ANGLE) {
-            // Move arm down
-            motor1.set(ARM_DOWN_SPEED);
-            motor2.set(ARM_DOWN_SPEED);
-        } else {
-            // Stop the motors if the arm is goody
-            motor1.set(0.0);
-            motor2.set(0.0);
+            if (currentAngle < DESIRED_ANGLE) {
+                // Move arm up
+                motor1.set(ARM_UP_SPEED);
+                motor2.set(ARM_UP_SPEED);
+            } else if (currentAngle > DESIRED_ANGLE) {
+                // Move arm down
+                motor1.set(ARM_DOWN_SPEED);
+                motor2.set(ARM_DOWN_SPEED);
+            } else {
+                // Stop the motors if the arm is good
+                motor1.set(0.0);
+                motor2.set(0.0);
+            }
         }
 
+        @Override
+        public void end(boolean interrupted) {
+            // Code to run when the command ends
+        }
+
+        @Override
+        public boolean isFinished() {
+            return false; // Modify this based on your command's end condition
+        }
     }
 
     private double armEncoder() {
+        // Implement your actual encoder reading logic here
         return 0;
     }
-
 }
