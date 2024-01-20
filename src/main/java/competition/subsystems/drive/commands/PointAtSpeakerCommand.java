0b6dd3a8aca3f7a6bb5aa29c690b2335916cc261
junyu101 <junyuzhu2007@gmail.com>
2024-01-20 11:38:44 -0800

    Update PointAtSpeakerCommand.java

--- src/main/java/competition/subsystems/drive/commands/PointAtSpeakerCommand.java
+++ src/main/java/competition/subsystems/drive/commands/PointAtSpeakerCommand.java
@@ -5,10 +5,9 @@ import competition.subsystems.pose.PoseSubsystem;
 import edu.wpi.first.math.geometry.Pose2d;
 import xbot.common.command.BaseCommand;
 import xbot.common.math.XYPair;
-<<<<<<< Updated upstream
+
 import xbot.common.subsystems.drive.control_logic.HeadingModule;
-=======
->>>>>>> Stashed changes
+
 import java.lang.Math;
 
 public class PointAtSpeakerCommand extends BaseCommand {
@@ -16,17 +15,16 @@ public class PointAtSpeakerCommand extends BaseCommand {
     PoseSubsystem pose;
     DriveSubsystem drive;
     XYPair speakerPosition = new XYPair(-0.0381,5.547868);
-<<<<<<< Updated upstream
+
     Pose2d currentPosition;
     HeadingModule headingModule;
 
-    public PointAtSpeakerCommand(){
+    public PointAtSpeakerCommand(DriveSubsystem drive, PoseSubsystem pose, HeadingModule.HeadingModuleFactory headingModuleFactory){
         
     }
 
-=======
-    Pose2d currentPosition = pose.getCurrentPose2d();
->>>>>>> Stashed changes
+
+
     double angle;
     @Override
     public void initialize() {
@@ -36,10 +34,8 @@ public class PointAtSpeakerCommand extends BaseCommand {
     @Override
     public void execute() {
         currentPosition = pose.getCurrentPose2d();
-<<<<<<< Updated upstream
+
         angle = (90 + (180 - Math.tan((currentPosition.getX() - speakerPosition.x) / (currentPosition.getY() - speakerPosition.y))));
-=======
-        angle = 90 + (180 - Math.tan(currentPosition.getX() - speakerPosition.x) / (currentPosition.getY() - speakerPosition.y) );
->>>>>>> Stashed changes
+
     }
 }
