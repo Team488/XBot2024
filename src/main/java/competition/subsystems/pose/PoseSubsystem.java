99c6b59dfabea7c3b596e68b52c68d36fe8bd0c9
anthonytrenh <trenhanthony@gmail.com>
2024-01-20 11:35:45 -0800

    Added note positions to PoseSubsystem, NoteMap addNote now refers to PoseSubsystem notes rather than making new ones

--- src/main/java/competition/subsystems/pose/PoseSubsystem.java
+++ src/main/java/competition/subsystems/pose/PoseSubsystem.java
@@ -52,6 +52,14 @@ public class PoseSubsystem extends BasePoseSubsystem {
     private final BooleanProperty allianceAwareFieldProp;
     private final BooleanProperty useVisionForPoseProp;
     private final Latch useVisionToUpdateGyroLatch;
+    public static Pose2d SpikeTop = new Pose2d(2.8956, 7.0012, new Rotation2d());
+    public static Pose2d SpikeMiddle = new Pose2d(2.8956, 5.5534, new Rotation2d());
+    public static Pose2d SpikeBottom = new Pose2d(2.8956, 4.1056, new Rotation2d());
+    public static Pose2d CenterLine1 = new Pose2d(8.2956, 7.4584, new Rotation2d());
+    public static Pose2d CenterLine2 = new Pose2d(8.2956, 5.7820, new Rotation2d());
+    public static Pose2d CenterLine3 = new Pose2d(8.2956, 4.1056, new Rotation2d());
+    public static Pose2d CenterLine4 = new Pose2d(8.2956, 2.4292, new Rotation2d());
+    public static Pose2d CenterLine5 = new Pose2d(8.2956, 0.7528, new Rotation2d());
 
     private DoubleProperty matchTime;
 
