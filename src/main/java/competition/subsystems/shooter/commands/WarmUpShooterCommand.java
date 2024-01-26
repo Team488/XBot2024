cd52dcc3f74981cef87b5bfa6f102302f9e53eaa
rzhai2 <rzhai2@jh.edu>
2024-01-25 20:01:46 -0800

    warm up shooter command draft

---
+++ src/main/java/competition/subsystems/shooter/commands/WarmUpShooterCommand.java
@@ -0,0 +1,40 @@
+package competition.subsystems.shooter.commands;
+import competition.subsystems.shooter.ShooterWheelSubsystem;
+import xbot.common.command.BaseSetpointCommand;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+import javax.inject.Inject;
+
+public class WarmUpShooterCommand extends BaseSetpointCommand {
+
+    ShooterWheelSubsystem shooter;
+
+    private Supplier<ShooterWheelSubsystem.TargetRPM> targetRPMSupplier;
+
+    @Inject
+    public WarmUpShooterCommand(ShooterWheelSubsystem shooter) {
+        super(shooter);
+        this.shooter = shooter;
+        addRequirements(shooter);
+    }
+
+    public void setTargetRpm(Supplier<ShooterWheelSubsystem.TargetRPM> targetRPMSupplier) {
+        this.targetRPMSupplier = targetRPMSupplier;
+    }
+
+    public void initialize() {
+        shooter.setTargetRPM(this.targetRPMSupplier.get());
+        shooter.setMaintainerIsAtGoal(false);
+    }
+
+    @Override
+    public void execute() {
+        // No-op. Wait for the arms to get to the target.
+    }
+
+    @Override
+    public boolean isFinished() {
+        return shooter.isMaintainerAtGoal();
+    }
+}
