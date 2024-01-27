ea7cb9fb0f9b658bad84432a0988958f669368de
Alex Schokking <aschokking@gmail.com>
2024-01-27 10:58:17 -0800

    Add missing import

--- src/main/java/competition/subsystems/pose/PoseSubsystem.java
+++ src/main/java/competition/subsystems/pose/PoseSubsystem.java
@@ -4,6 +4,7 @@ import competition.subsystems.drive.DriveSubsystem;
 import competition.subsystems.vision.VisionSubsystem;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
