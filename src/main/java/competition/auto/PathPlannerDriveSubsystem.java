2294482dbd953d810cafab30c7b9981b5a16e56d
kujo27 <johnkuang783@gmail.com>
2024-02-01 19:33:00 -0800

    merge b4 commit

--- src/main/java/competition/auto/PathPlannerDriveSubsystem.java
+++ src/main/java/competition/auto/PathPlannerDriveSubsystem.java
@@ -37,9 +37,9 @@ public class PathPlannerDriveSubsystem extends BaseSubsystem {
                 this::getChassisSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
                 this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
                 new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
-                        new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
-                        new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
-                        0.2, // Max module speed, in m/s
+                        new PIDConstants(0.7, 0.0, 0.2), // Translation PID constants
+                        new PIDConstants(0, 0.0, 0.0), // Rotation PID constants
+                        4.5, // Max module speed, in m/s
                         0.4, // Drive base radius in meters. Distance from robot center to furthest module.
                         new ReplanningConfig() // Default path replanning config. See the API for the options here
                 ),
