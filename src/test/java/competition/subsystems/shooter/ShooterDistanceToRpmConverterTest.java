6cc7d64ba32c7988164060c836b235f52876e9cd
junyu101 <junyuzhu2007@gmail.com>
2024-02-03 10:51:21 -0800

    Added edge case testing

--- src/test/java/competition/subsystems/shooter/ShooterDistanceToRpmConverterTest.java
+++ src/test/java/competition/subsystems/shooter/ShooterDistanceToRpmConverterTest.java
@@ -33,5 +33,9 @@ public class ShooterDistanceToRpmConverterTest extends BaseCompetitionTest {
         assertEquals(725,converter.getRPMForDistance(1.5),0.00001);
         assertEquals(2850,converter.getRPMForDistance(9),0.00001);
 
+        //testing when distance is less than the lowest or greater than the highest recorded data point(should return zero?? Unless we should return something else)
+        assertEquals(0,converter.getRPMForDistance(0.5),0.00001);
+        assertEquals(0,converter.getRPMForDistance(200),0.00001);
+
     }
 }
