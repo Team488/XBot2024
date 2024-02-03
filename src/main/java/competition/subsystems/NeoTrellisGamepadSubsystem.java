7222c36830450e19846098e4e231e841ad7b89f8
Stephen Just <stephenjust@gmail.com>
2024-02-03 10:29:50 -0800

    Add command factory methods

--- src/main/java/competition/subsystems/NeoTrellisGamepadSubsystem.java
+++ src/main/java/competition/subsystems/NeoTrellisGamepadSubsystem.java
@@ -1,6 +1,8 @@
 package competition.subsystems;
 
 import edu.wpi.first.wpilibj.util.Color8Bit;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import xbot.common.advantage.DataFrameRefreshable;
 import xbot.common.command.BaseSubsystem;
 
@@ -61,5 +63,32 @@ public class NeoTrellisGamepadSubsystem extends BaseSubsystem implements DataFra
     public void refreshDataFrame() {
         aKitLog.record("ButtonColors",
                 buttonColors.stream().map(Color8Bit::toHexString).toArray(String[]::new));
+    }
+
+    /**
+     * Get a command to clear the button colors
+     * @return A command to clear the button colors
+     */
+    public Command getClearCommand() {
+        return new InstantCommand(this::clearButtonColors);
+    }
+
+    /**
+     * Get a command to fill the button colors with a single color
+     * @param color The color to fill the buttons with
+     * @return A command to fill the button colors with a single color
+     */
+    public Command getFillCommand(Color8Bit color) {
+        return new InstantCommand(() -> fillButtonColors(color));
+    }
+
+    /**
+     * Get a command to set the color of a button
+     * @param buttonIndex The index of the button to set
+     * @param color The color to set the button to
+     * @return A command to set the color of a button
+     */
+    public Command getSetButtonColorCommand(int buttonIndex, Color8Bit color) {
+        return new InstantCommand(() -> setButtonColor(buttonIndex, color));
     }
 }
