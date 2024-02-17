c0ccd7e01e3512cb3be44ce12ea9340406f1be6a
junyu101 <junyuzhu2007@gmail.com>
2024-02-17 10:49:48 -0800

    Updated WaitForArm to extend baseWaitFormMaintainer

--- src/main/java/competition/subsystems/arm/commands/WaitForArmToBeAtGoalCommand.java
+++ src/main/java/competition/subsystems/arm/commands/WaitForArmToBeAtGoalCommand.java
@@ -3,16 +3,26 @@ package competition.subsystems.arm.commands;
 import competition.subsystems.arm.ArmSubsystem;
 import competition.subsystems.arm.ArmSubsystem_Factory;
 import xbot.common.command.BaseCommand;
+import xbot.common.command.BaseWaitForMaintainerCommand;
+import xbot.common.controls.sensors.XTimer;
+import xbot.common.properties.DoubleProperty;
+import xbot.common.properties.PropertyFactory;
 
-public class WaitForArmToBeAtGoalCommand extends BaseCommand {
-    ArmSubsystem arm;
-    public WaitForArmToBeAtGoalCommand(ArmSubsystem arm){
-        addRequirements(arm);
+import javax.inject.Inject;
+
+public class WaitForArmToBeAtGoalCommand extends BaseWaitForMaintainerCommand {
+    final ArmSubsystem arm;
+    private DoubleProperty timeoutProperty;
+    private double startTime;
+    @Inject
+    public WaitForArmToBeAtGoalCommand(ArmSubsystem arm, PropertyFactory pf, double defaultTimeout){
+        super(arm,pf,defaultTimeout);
+
         this.arm = arm;
     }
     @Override
     public void initialize() {
-        //nothing to do
+        this.startTime = XTimer.getFPGATimestamp();
     }
 
     @Override
@@ -22,6 +32,6 @@ public class WaitForArmToBeAtGoalCommand extends BaseCommand {
 
     @Override
     public boolean isFinished() {
-        return arm.isMaintainerAtGoal();
+        return super.isFinished();
     }
 }
