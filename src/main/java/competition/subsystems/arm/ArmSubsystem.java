57aea814e64832c97c04c99766a79cfffe12e2e4
Rong <ronrzhu@gmail.com>
2024-02-17 10:30:07 -0800

    Fixed error

--- src/main/java/competition/subsystems/arm/ArmSubsystem.java
+++ src/main/java/competition/subsystems/arm/ArmSubsystem.java
@@ -340,8 +340,8 @@ public class ArmSubsystem extends BaseSetpointSubsystem<Double> implements DataF
         boolean lowerHit = false;
 
         if (contract.isArmReady()) {
-            upperHit = motor.getForwardLimitSwitchPressed(SparkLimitSwitch.Type.kNormallyOpen);
-            lowerHit = motor.getReverseLimitSwitchPressed(SparkLimitSwitch.Type.kNormallyOpen);
+            upperHit = motor.getForwardLimitSwitchPressed();
+            lowerHit = motor.getReverseLimitSwitchPressed();
         }
 
         if (upperHit && lowerHit) {
