f72be631058b6fbaec3cf571a0b14c87dc628657
Rong <ronrzhu@gmail.com>
2024-02-29 20:23:13 -0800

    Updated lights subsystem to support serial data
    We need expertise on more event listeners though.

--- src/main/java/competition/subsystems/lights/LightSubsystem.java
+++ src/main/java/competition/subsystems/lights/LightSubsystem.java
@@ -6,7 +6,11 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import competition.electrical_contract.ElectricalContract;
+import competition.subsystems.collector.CollectorSubsystem;
+import competition.subsystems.shooter.ShooterWheelSubsystem;
+import competition.subsystems.shooter.ShooterWheelTargetSpeeds;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.SerialPort;
 import org.littletonrobotics.junction.Logger;
 import xbot.common.command.BaseSubsystem;
 import xbot.common.controls.actuators.XDigitalOutput;
@@ -16,73 +20,95 @@ import xbot.common.properties.BooleanProperty;
 import xbot.common.properties.Property;
 import xbot.common.properties.PropertyFactory;
 import xbot.common.properties.StringProperty;
+import xbot.common.subsystems.autonomous.AutonomousCommandSelector;
 
 @Singleton
 public class LightSubsystem extends BaseSubsystem {
-    final XDigitalOutput dio0;
-    final XDigitalOutput dio1;
-    final XDigitalOutput dio2;
-    final XDigitalOutput dio3;
-    final XDigitalOutput dio4;
-    final XDigitalOutput cubeDio;
 
-    final XDigitalOutput[] dioOutputs;
+    SerialPort serialPort;
     private int loopCounter;
     private final int loopMod = 5;
-    private String chosenState;
+    AutonomousCommandSelector autonomousCommandSelector;
+    ShooterWheelSubsystem shooter;
+    CollectorSubsystem collector;
 
     public enum LightsStateMessage{
-        //at this time values are here as placeholders
-        RobotNotBooted(1),
-        RobotCollectingNote(22),
-        RobotHoldingNote(30),
-        RobotAuto(3),
-        RobotEnabled(2),
-        FacingSpeaker(4),
-        FacingAmp(5),
-        FacingSource(6);
-        private int value;
 
-        private LightsStateMessage(final int value) {
+        RobotNoCode("31"), // Likely to be unnecessary
+        DisabledWithoutAuto("32"),
+        DisabledWithAuto("33"),
+        RobotEnabled("34"),
+        AmpSignal("1"), // TODO: OI team please add a toggle!
+        ReadyToShoot("2"),
+        RobotContainsNote("3"),
+        VisionSeesNote("4");
+
+        LightsStateMessage(final String value) {
             this.value = value;
         }
 
-        public int getValue() {
+        private String value;
+        public String getValue() {
             return value;
         }
-
-
-
     }
     @Inject
-    public LightSubsystem(XDigitalOutputFactory digitalOutputFactory, XPWMFactory pwmFactory,
-                          PropertyFactory pf, ElectricalContract contract) {
-        dio0 = digitalOutputFactory.create(contract.getLightsDio0().channel);
-        dio1 = digitalOutputFactory.create(contract.getLightsDio1().channel);
-        dio2 = digitalOutputFactory.create(contract.getLightsDio2().channel);
-        dio3 = digitalOutputFactory.create(contract.getLightsDio3().channel);
-        dio4 = digitalOutputFactory.create(contract.getLightsDio4().channel);
-        cubeDio = digitalOutputFactory.create(contract.getLightsCubeDio().channel);
-        dioOutputs = new XDigitalOutput[] { dio0, dio1, dio2, dio3, dio4 };
-
+    public LightSubsystem(PropertyFactory pf, AutonomousCommandSelector autonomousCommandSelector,
+                          ShooterWheelSubsystem shooter, CollectorSubsystem collector) {
+        serialPort = new SerialPort(115200, SerialPort.Port.kUSB, 64);
+        // Not sure about these, is pf necessary here?
         pf.setPrefix(this);
         pf.setDefaultLevel(Property.PropertyLevel.Debug);
-        chosenState = "Nothing Yet Set";
-    }
-    public void periodic() {
-        LightsStateMessage currentState = LightsStateMessage.RobotNotBooted;
 
-        int stateValue = currentState.getValue();
-        for (int i = 0; i < dioOutputs.length; i++) {
-            dioOutputs[i].set(((stateValue & (1 << i)) != 0));
+        this.autonomousCommandSelector = autonomousCommandSelector;
+        this.collector = collector;
+        this.shooter = shooter;
+    }
+
+    @Override
+    public void periodic() {
+        // Runs period every 1/10 of a second
+        if (this.loopCounter++ % loopMod != 0) {
+            return;
         }
 
-        aKitLog.record("ArduinoState", currentState.toString());
-        aKitLog.record("DIO0", dio0.get());
-        aKitLog.record("DIO1", dio1.get());
-        aKitLog.record("DIO2", dio2.get());
-        aKitLog.record("DIO3", dio3.get());
-        aKitLog.record("DIO4", dio4.get());
-        aKitLog.record("IsConeDIO", cubeDio.get());
+        boolean dsEnabled = DriverStation.isEnabled();
+        LightsStateMessage currentState = LightsStateMessage.RobotNoCode;
+        ShooterWheelTargetSpeeds shooterWheel = shooter.getCurrentValue();
+
+        // Always assumes disabled without auto because we don't have AutonomousOracle >:(
+        // Needs to implement vision as well
+        // Not sure about if the way we are checking the shooter is correct
+        if (!dsEnabled) {
+//            if (autonomousCommandSelector.getCurrentAutonomousCommand() != null) {
+//                if (oracle.isAutoCustomized()) {
+//                    currentState = LightsStateMessage.RobotDisabledWithCustomizedAuto;
+//                } else {
+//                    currentState = LightsStateMessage.RobotDisabledWithBasicAuto;
+//                }
+//            } else {
+//                currentState = LightsStateMessage.RobotDisabledNoAuto;
+//            }
+            currentState = LightsStateMessage.DisabledWithoutAuto;
+
+        } else if (shooter.isMaintainerAtGoal() &&
+                shooterWheel.lowerWheelsTargetRPM != 0 &&
+                shooterWheel.upperWheelsTargetRPM != 0) {
+            currentState = LightsStateMessage.ReadyToShoot;
+
+        } else if (collector.getGamePieceReady()) {
+            currentState = LightsStateMessage.RobotContainsNote;
+
+        } else {
+            currentState = LightsStateMessage.RobotEnabled;
+        }
+
+        String stateValue = currentState.getValue();
+
+        // Write serial data to lights
+        serialPort.writeString(stateValue + "\r\n");
+        serialPort.flush();
+
+        aKitLog.record("LightState", currentState.toString());
     }
 }
