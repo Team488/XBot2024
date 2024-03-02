1bb426329f562770af3706bdb5ead9be5b49c096
John Gilbert <johngilb@gmail.com>
2024-03-02 00:44:21 -0800

    Forgot @Inject

--- src/main/java/competition/subsystems/arm/commands/ManualHangingModeCommand.java
+++ src/main/java/competition/subsystems/arm/commands/ManualHangingModeCommand.java
@@ -3,10 +3,13 @@ package competition.subsystems.arm.commands;
 import competition.subsystems.arm.ArmSubsystem;
 import xbot.common.command.BaseCommand;
 
+import javax.inject.Inject;
+
 public class ManualHangingModeCommand extends BaseCommand {
 
     ArmSubsystem arm;
 
+    @Inject
     public ManualHangingModeCommand(ArmSubsystem arm) {
         this.arm = arm;
         this.addRequirements(arm);
