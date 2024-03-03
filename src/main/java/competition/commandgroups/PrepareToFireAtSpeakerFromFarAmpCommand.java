73fb1d2dff3ea8f6f5e375a02efd0d7e020852a1
JohnGilb <johngilb@gmail.com>
2024-03-02 22:15:19 -0800

    Renamed file, converted to parallel command group

---
+++ src/main/java/competition/commandgroups/PrepareToFireAtSpeakerFromFarAmpCommand.java
@@ -0,0 +1,25 @@
+package competition.commandgroups;
+
+import competition.subsystems.arm.ArmSubsystem;
+import competition.subsystems.arm.commands.SetArmExtensionCommand;
+import competition.subsystems.shooter.ShooterWheelSubsystem;
+import competition.subsystems.shooter.commands.WarmUpShooterCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+
+import javax.inject.Inject;
+
+public class PrepareToFireAtSpeakerFromFarAmpCommand  extends ParallelCommandGroup {
+
+    @Inject
+    public PrepareToFireAtSpeakerFromFarAmpCommand(
+            WarmUpShooterCommand warmUpShooter,
+            SetArmExtensionCommand setArmExtension,
+            ArmSubsystem arm) {
+        warmUpShooter.setTargetRpm(ShooterWheelSubsystem.TargetRPM.TYPICAL);
+        setArmExtension.setTargetExtension(
+                arm.getUsefulArmPositionExtensionInMm(ArmSubsystem.UsefulArmPosition.PROTECTED_FAR_AMP_SHOT));
+
+        this.addCommands(warmUpShooter, setArmExtension);
+    }
+}
+
