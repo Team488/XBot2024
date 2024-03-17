dc436819023d95a6d98423caf11ffb76cfd560ac
Alex Schokking <aschokking@gmail.com>
2024-03-17 13:14:04 -0700

    make double checking more robust

--- src/main/java/competition/subsystems/arm/commands/ArmMaintainerCommand.java
+++ src/main/java/competition/subsystems/arm/commands/ArmMaintainerCommand.java
@@ -143,7 +143,7 @@ public class ArmMaintainerCommand extends BaseMaintainerCommand<Double> {
     @Override
     protected boolean additionalAtGoalChecks() {
         // if the safe arm value is preventing us from reaching our goal, say we're not at goal
-        if (arm.getTargetValue() != arm.getSafeTargetValue()) {
+        if (Math.abs(arm.getTargetValue() - arm.getSafeTargetValue()) > 0.001) {
             return false;
         }
         return true;
