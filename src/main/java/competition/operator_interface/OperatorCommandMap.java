d2ea5b137978b00bc04e34e0cadc8d0e8a9f1e4d
junyu101 <junyuzhu2007@gmail.com>
2024-03-20 17:41:11 -0700

    forgot to delete

--- src/main/java/competition/operator_interface/OperatorCommandMap.java
+++ src/main/java/competition/operator_interface/OperatorCommandMap.java
@@ -95,7 +95,6 @@ public class OperatorCommandMap {
                 .onFalse(cancelSpecialPointAtPosition);
 
         operatorInterface.driverGamepad.getPovIfAvailable(90).whileTrue(lineUpForHangingCommand);
-        operatorInterface.driverGamepad.getXboxButton(XboxButton.A).whileTrue(test1);
 
     }
 
