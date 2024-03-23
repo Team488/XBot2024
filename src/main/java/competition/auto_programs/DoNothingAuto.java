5da1304e27c2dac2e78fae3492a8516f4489abdb
JohnGilb <johngilb@gmail.com>
2024-03-23 13:54:25 -0700

    Add do nothing auto

---
+++ src/main/java/competition/auto_programs/DoNothingAuto.java
@@ -0,0 +1,13 @@
+package competition.auto_programs;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+import javax.inject.Inject;
+
+public class DoNothingAuto extends SequentialCommandGroup {
+
+    @Inject
+    public DoNothingAuto() {
+
+    }
+}
