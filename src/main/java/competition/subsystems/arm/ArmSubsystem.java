4c027a5954d7efa00fcf6b3bcda43bce93c73bb3
anthonytrenh <trenhanthony@gmail.com>
2024-03-31 13:46:07 -0700

    ramp value changed to 0.125

--- src/main/java/competition/subsystems/arm/ArmSubsystem.java
+++ src/main/java/competition/subsystems/arm/ArmSubsystem.java
@@ -171,7 +171,7 @@ public class ArmSubsystem extends BaseSetpointSubsystem<Double> implements DataF
         overallPowerClampForTesting = pf.createPersistentProperty("overallTestingPowerClamp", 0.45);
         maximumExtensionDesyncMm = pf.createPersistentProperty("MaximumExtensionDesyncMm", 6.0);
 
-        powerRampDurationSec = pf.createPersistentProperty("PowerRampDurationSec", 0.5);
+        powerRampDurationSec = pf.createPersistentProperty("PowerRampDurationSec", 0.125);
 
         pf.setDefaultLevel(Property.PropertyLevel.Debug);
         absoluteEncoderOffset = pf.createPersistentProperty(
