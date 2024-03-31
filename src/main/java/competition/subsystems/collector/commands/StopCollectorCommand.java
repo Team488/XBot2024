ce15138a2dbaad8c5e4888852852ca6b12772fdf
Alex Schokking <aschokking@gmail.com>
2024-03-31 11:43:18 -0700

    update all existing collector commands to use pid lock

--- src/main/java/competition/subsystems/collector/commands/StopCollectorCommand.java
+++ src/main/java/competition/subsystems/collector/commands/StopCollectorCommand.java
@@ -2,17 +2,18 @@ package competition.subsystems.collector.commands;
 
 import competition.subsystems.collector.CollectorSubsystem;
 import xbot.common.command.BaseCommand;
+import xbot.common.command.BaseSetpointCommand;
 
 import javax.inject.Inject;
 
 
-public class StopCollectorCommand extends BaseCommand {
+public class StopCollectorCommand extends BaseSetpointCommand {
     CollectorSubsystem collector;
 
     @Inject
     public StopCollectorCommand(CollectorSubsystem collector) {
+        super(collector);
         this.collector = collector;
-        addRequirements(collector);
     }
 
     @Override
--- src/main/java/competition/subsystems/shooter/commands/FireWhenReadyCommand.java
+++ src/main/java/competition/subsystems/shooter/commands/FireWhenReadyCommand.java
@@ -4,13 +4,14 @@ import competition.subsystems.arm.ArmSubsystem;
 import competition.subsystems.collector.CollectorSubsystem;
 import competition.subsystems.shooter.ShooterWheelSubsystem;
 import xbot.common.command.BaseCommand;
+import xbot.common.command.BaseSetpointCommand;
 import xbot.common.controls.sensors.XTimer;
 import xbot.common.properties.DoubleProperty;
 import xbot.common.properties.PropertyFactory;
 
 import javax.inject.Inject;
 
-public class FireWhenReadyCommand extends BaseCommand {
+public class FireWhenReadyCommand extends BaseSetpointCommand {
     final ShooterWheelSubsystem wheel;
     final ArmSubsystem arm;
     final CollectorSubsystem collector;
@@ -18,7 +19,7 @@ public class FireWhenReadyCommand extends BaseCommand {
     @Inject
     public FireWhenReadyCommand(ShooterWheelSubsystem wheel, ArmSubsystem arm, CollectorSubsystem collector,
                                 PropertyFactory pf) {
-        addRequirements(collector);
+        super(collector);
         this.wheel = wheel;
         this.arm = arm;
         this.collector = collector;
