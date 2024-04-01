f122235dcc3f21d7b87c21b1754fc14f338d0f2f
JohnGilb <johngilb@gmail.com>
2024-03-31 17:40:31 -0700

    Improve Autonomous Note Detection and Collection (#302)
    * First pass on logic
    
    * Start refactor
    
    * Add rotation search
    
    * improvements
    
    * Last polish
    
    * Use newer commonlib
    
    * update defaults
    
    * Clean up OI

---
+++ src/main/java/competition/subsystems/vision/NoteAcquisitionMode.java
@@ -0,0 +1,10 @@
+package competition.subsystems.vision;
+
+public enum NoteAcquisitionMode {
+    BlindApproach,
+    VisionApproach,
+    VisionTerminalApproach,
+    BackAwayToTryAgain,
+    SearchViaRotation,
+    GiveUp
+}
